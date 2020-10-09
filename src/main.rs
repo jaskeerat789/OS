598844baf039af28b17a2768885e8950e8064e87
jaskeerat singh <jaskee789@gmail.com>
2020-10-10 01:58:17 +0530

    VGA Buffer

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
-    loop{}
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop{}
+    vga_buffer::print_something();
+       
+    loop {}
 }
