4f6c8ffec92b389183f473fcffa2b26d10ead773
Jaskeerat Singh Randhawa <jaskee789@gmail.com>
2020-10-19 22:17:30 +0530

    Stack Overflow: making stack to iverflow

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,13 @@ pub extern "C" fn _start() -> ! {
 
     os::init();
     // x86_64::instructions::interrupts::int3();
-    unsafe {
-        *(0xdeadbeef as *mut u64) = 42;
+    
+    fn stack_overflow() {
+        stack_overflow();
     }
 
+    stack_overflow();
+    
     #[cfg(test)]
     test_main();
     
