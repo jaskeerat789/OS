153cb3edab66317f44e8b3e2f20f8ad5a4b17cd0
Jaskeerart Singh <jaskee789@gmail.com>
2020-10-27 21:37:30 +0530

    Hardware Interrupt: implement keyboard scancode to print character on VGA

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -29,6 +29,9 @@ lazy_static! {
         idt[InterruptIndex::Timer.as_usize()]
             .set_handler_fn(timer_interrupt_handler);       
         
+        // Keyboard Interrupt handler entry
+        idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        
         // return IDT 
         idt
     };
@@ -60,11 +63,38 @@ extern "x86-interrupt" fn timer_interrupt_handler(
     }
 }
 
+extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut InterruptStackFrame){
+    
+    use spin::Mutex;
+    use x86_64::instructions::port::Port;
+    use pc_keyboard::{layouts, DecodedKey, HandleControl, Keyboard, ScancodeSet1};
+    
+    lazy_static! {
+        static ref KEYBOARD: Mutex<Keyboard<layouts::Us104Key, ScancodeSet1>> = Mutex::new(Keyboard::new(layouts::Us104Key, ScancodeSet1, HandleControl::Ignore));
+    }
+    let mut keyboard = KEYBOARD.lock();
+    let mut port = Port::new(0x60);
+
+    let scancode: u8 = unsafe { port.read() };
+    if let Ok(Some(key_event)) = keyboard.add_byte(scancode){
+        if let Some(key) = keyboard.process_keyevent(key_event){
+            match key{
+                DecodedKey::Unicode(character) => print!("{}",character),
+                DecodedKey::RawKey(key) => print!("{:?}", key),
+            }
+        }
+    }
+
+    unsafe {
+        PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
+    }
+}
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum InterruptIndex {
     Timer = PIC_1_OFFSET,
+    Keyboard,
 }
 
 impl InterruptIndex {
