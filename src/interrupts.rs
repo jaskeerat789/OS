3b723a343ea836dacfe5fd622c8ddc760088789b
jaskeerat singh <jaskee789@gmail.com>
2020-10-30 02:39:14 +0530

    Paging: Page Fault handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,16 +1,16 @@
 use crate::gdt;
 use crate::print;
 use crate::println;
+use crate::hlt_loop;
 
 use spin;
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 pub const PIC_1_OFFSET: u8 = 32;
 pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
-pub static PICS: spin::Mutex<ChainedPics> =
-    spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
+pub static PICS: spin::Mutex<ChainedPics> = spin::Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -32,6 +32,9 @@ lazy_static! {
         // Keyboard Interrupt handler entry
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
         
+        // Page fault Interrupt 
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         // return IDT 
         idt
     };
@@ -61,6 +64,19 @@ extern "x86-interrupt" fn timer_interrupt_handler(
         PICS.lock()
             .notify_end_of_interrupt(InterruptIndex::Timer.as_u8());
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+){
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}",Cr2::read());
+    println!("Error Code: {:?}",error_code);
+    println!("{:?}",stack_frame);
+    hlt_loop();
 }
 
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: &mut InterruptStackFrame){
--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,33 @@
 #![test_runner(os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use core::panic::PanicInfo;
 use os::println;
+use core::panic::PanicInfo;
+use x86_64::registers::control::Cr3;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
     os::init();
-    // x86_64::instructions::interrupts::int3();
     
+    // let ptr = 0x204920 as *mut u32;
+    // unsafe { let x = *ptr; }
+    // println!("read worked");
+    // unsafe { *ptr = 42; }
+    // println!("write worked");
+
+    let (level_4_page_table,_) = Cr3::read();
+    println!("Level 4 page table at: {:?}",level_4_page_table.start_address());
+
     #[cfg(test)]
     test_main();
     
     println!("It did not crash!!");
-        os::hlt_loop();
+    os::hlt_loop();
 }
 
-/// This function is called on panic.
+// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
